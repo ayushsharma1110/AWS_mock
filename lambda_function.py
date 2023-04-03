3bf44d30c73af8c6027369b4d3cf336a95fecef2
ayushsharma1110 <ayushbsharma@yahoo.co.in>
2023-04-03 13:45:31 +0530

    First Commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,15 @@
 import json
+
 from datetime import datetime
 
 now = datetime.now()
 current_time =now.strftime("%H:%M:%S")
 
 def lambda_handler (event, context):
-    print("Hello")
+    print("Hello!")
     print("Current Time is", current_time)
-
 
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from lambda')
     }
-
-
